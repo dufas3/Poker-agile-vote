a0ff0f1f9b320313434e8c587ac2bceb1fe78efd
Matas Razumas <matas.razumas@stud.kitm.lt>
2023-02-22 19:12:33 +0000

    Merged PR 40: Fixed Trash files and displaying player list
    Fixed Trash files. wont show up in github desktop...
    added player list functionality (check PlayerList.js)

---
+++ PokerFace/Data/Repositories/UsersRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokerFace.Data.Entities;
+
+namespace PokerFace.Data.Repositories
+{
+    public class UsersRepository
+    {
+        ApplicationDbContext context { get; set; }
+        public static List<User> StaticUsers { get; set; }
+        public UsersRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+            StaticUsers = new List<User>
+           {
+                new Data.Entities.User
+                {
+                    Id = 1,
+                    Name = "Matas",
+                    Password = "Passwd"
+
+                },
+                 new Data.Entities.User
+                {
+                    Id = 2,
+                    Name = "Lukas",
+                    Password = "Passw2d"
+
+                }
+           };
+        }
+        public async Task<List<User>> GetAllAsync()
+        {
+            return await Task.FromResult(context.Users.ToList());
+        }
+    }
+}
--- PokerFace/ui/src/components/poker/PlayerList.js
+++ PokerFace/ui/src/components/poker/PlayerList.js
@@ -1,11 +1,27 @@
-import './Poker.css'
-import UserIcon from '../../imgs/user.png';
+import "./Poker.css";
+import UserIcon from "../../imgs/user.png";
+import { useEffect, useState } from "react";
 function PlayerList() {
-    return (
-        <div className="player-list border rounded bg-light">
-            <h1>Player list</h1>
-        </div>
-    )
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    //get all users api
+    fetch("https://localhost:5001/api/user/getall")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setUsers(data);
+      });
+  }, []);
+
+  return (
+    <div className="player-list border rounded bg-light">
+      <h1>Player list</h1>
+      {users.map((user) => (
+        <h4>{user.name}</h4>
+      ))}
+    </div>
+  );
 }
 
 export default PlayerList;
