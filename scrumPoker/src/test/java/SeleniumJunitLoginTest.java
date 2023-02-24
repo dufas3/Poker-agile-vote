32becda869dadfea60b54080c5165f0e0ef67417
Danguolė Veličkaitė <danguole.velickaite@stud.kitm.lt>
2023-02-24 15:53:21 +0000

    Merged PR 42: Set up full project
    Set up full project

---
+++ scrumPoker/src/test/java/SeleniumJunitLoginTest.java
@@ -0,0 +1,49 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SeleniumJunitLoginTest {
+
+    //aprasomi veiksmai, kurie bus atliekami pries kiekvino testo paleidima
+    //describing actions, which will be performed before every test launch
+    @Before
+    public void setup() {
+        Selenium.setup(Selenium.POKER_URL);
+    }
+
+    @Test
+
+    public void loginPlayerPositiveTest() {
+        Selenium.loginPlayer();
+        String expectedResults = "Antanukas";
+        Assert.assertEquals(Selenium.getLoginResults(), expectedResults);
+
+    }
+
+    @Test
+    public void loginPlayerNegativeTest() {
+        Selenium.loginPlayer();
+        String expectedResults = "Business";
+        Assert.assertNotEquals(Selenium.getLoginResults(), expectedResults);
+
+    }
+    @Test
+
+    public void loginModeratorPositiveTest() {
+        Selenium.loginModerator();
+        String expectedResults = "testemail@gmail.com";
+        Assert.assertEquals(Selenium.getLoginResults(), expectedResults);
+
+    }
+
+    @Test
+    public void loginModeratorNegativeTest() {
+        Selenium.loginModerator();
+        String expectedResults = "Business";
+        Assert.assertNotEquals(Selenium.getLoginResults(), expectedResults);
+
+    }
+
+
+}
