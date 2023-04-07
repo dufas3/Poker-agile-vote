652f249c639ae57a6dba97d90cccd4a7ee35bf52
Danguolė Veličkaitė <danguole.velickaite@stud.kitm.lt>
2023-04-07 10:03:54 +0000

    Merged PR 200: WaitMessageFixesAzure
    WaitMessageFixesAzure

--- scrumPokerProject/src/test/java/ModeratorSettings.java
+++ scrumPokerProject/src/test/java/ModeratorSettings.java
@@ -202,6 +202,7 @@ public class ModeratorSettings {
     }
 
     public static void waitPhaseMessageResults(String text) {
+        Setup.waitForElementToAppear(PHASE_MESSAGE);
         WebElement message = Setup.browser.findElement(PHASE_MESSAGE);
         new WebDriverWait(Setup.browser, Duration.ofSeconds(20), Duration.ofMillis(50)).until(ExpectedConditions.textToBePresentInElement(message, text));
     }
